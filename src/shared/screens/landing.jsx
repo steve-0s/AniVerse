2845e34846161dfaf1042a657fe43b958208c531
Stevens Anthony Fabriga <steve-0s@Chiggas-MacBook-Pro.local>
2026-04-17 15:03:11 +0800

    changed the Palette for the manga side

--- src/shared/screens/landing.jsx
+++ src/shared/screens/landing.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import SakuraFall from '../effects/SakuraFall.jsx';
-import SearchBar from '../../../shared/components/searchbar.jsx';
+import SakuraFall from '../../features/app/effects/SakuraFall.jsx';
+import SearchBar from '../components/searchbar.jsx';
 
 const Landing = () => {
   const navigate = useNavigate();
